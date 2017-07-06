b01d6e7dfd47e9a4822305ce57fe8ba689501023
Marcus Posey <marcusposey@gmail.com>
2017-07-06 09:40:09 -0500

    Update zero initialization syntax to fix compiler warnings

--- socket.cpp
+++ socket.cpp
@@ -64,7 +64,7 @@ bool plain_socket::set_hostname(const char* sAddr)
 	sPort[0] = '\0';
 	sPort++;
 
-	addrinfo hints = { 0 };
+	addrinfo hints = {};
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
